c691bb61dc363d9afa594d4a37b643ceaae5839b
Javier Alcoriza <jacoriza@gmail.com>
2020-01-05 16:37:08 +0100

    First version using BeatifulSoap

--- myfile.py
+++ myfile.py
@@ -2,7 +2,11 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 html = urlopen("https://www.python.org/")
-res = BeautifulSoup(html.read(),"html5lib")
-#res = BeautifulSoup(html.read());
+#res = BeautifulSoup(html.read(),"html5lib")
+res = BeautifulSoup(html.read(), "html.parser")
+
+
+# import ipdb; ipdb.set_trace() # BREAKPOINT
+
 print(res.title)
 
