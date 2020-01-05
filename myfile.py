5d096063f3f86c8ddd544e3bf43402662b745ca4
Javier Alcoriza <jacoriza@gmail.com>
2020-01-05 16:55:17 +0100

    Added exceptions in urlopen()

--- myfile.py
+++ myfile.py
@@ -1,12 +1,23 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
 from bs4 import BeautifulSoup
 
-html = urlopen("https://www.python.org/")
-#res = BeautifulSoup(html.read(),"html5lib")
-res = BeautifulSoup(html.read(), "html.parser")
+#website = "https://www.python.org"
+website = "https://www.kkpis.org"
 
-
-# import ipdb; ipdb.set_trace() # BREAKPOINT
-
-print(res.title)
+try:
+    html = urlopen(website)
+    
+except HTTPError as e:
+    print(e)
+    
+except URLError:
+    print("Server down or incorrect domain")
+    
+else:
+    #res = BeautifulSoup(html.read(),"html5lib")
+    res = BeautifulSoup(html.read(), "html.parser")
+    # import ipdb; ipdb.set_trace() # BREAKPOINT
+    print(res.title)
 
